b5018858d6a38000e8db0eabaca35524915d5b05
ardiansyahirwan <ardiansyahirwan923@gmail.com>
2024-08-15 15:14:18 +0700

    feat:remove dashboard filter

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -11,47 +11,14 @@ use Filament\Forms\Components\Section;
 use Filament\Forms\Form;
 use Filament\Pages\Dashboard as BaseDashboard;
 use Filament\Pages\Dashboard\Actions\FilterAction;
-use Filament\Pages\Dashboard\Concerns\HasFilters;
-use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class Dashboard extends BaseDashboard
 {
-    use HasFilters;
-    use InteractsWithPageFilters;
 
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
     protected static string $view = 'filament.pages.dashboard';
     protected static string $routePath = 'dashboard';
-
-    public function filtersForm(Form $form): Form
-    {
-        return $form
-            ->schema([
-                Section::make()
-                    ->schema([
-                        DatePicker::make('startDate'),
-                        DatePicker::make('endDate'),
-                    ])
-                    ->columns(3),
-            ]);
-    }
-
-    public function getStats(): array
-    {
-        $startDate = $this->filters['startDate'] ?? null;
-        $endDate = $this->filters['endDate'] ?? null;
-
-        return [
-            Stat::make(
-                label: 'Total Pet Patient',
-                value: Patient::query()
-                    ->when($startDate, fn (Builder $query) => $query->whereDate('created_at', '>=', $startDate))
-                    ->when($endDate, fn (Builder $query) => $query->whereDate('created_at', '<=', $endDate))
-                    ->count(),
-            ),
-        ];
-    }
 
     protected function getHeaderWidgets(): array
     {
